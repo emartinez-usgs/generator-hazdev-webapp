522f15ffc7653c018c798dc2f54db634ae8a87c2
Eric Martinez <emartinez@usgs.gov>
2015-03-17 09:13:04 -0600

    Changed ports around. Added test config stuff.

--- app/templates/gruntconfig/config.js
+++ app/templates/gruntconfig/config.js
@@ -5,9 +5,11 @@ var config = {
   build: '.build',
   buildPort: 8000,
   dist: 'dist',
-  distPort: 8001,
+  distPort: 8003,
   liveReloadPort: 8009,
-  templatePort: 8008
+  templatePort: 8008,
+  test: 'test',
+  testPort: 8001
 };
 
 module.exports = config;
