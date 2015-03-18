916626e16a21154cb659a0596c606f95cdcf4395
Eric Martinez <emartinez@usgs.gov>
2015-03-18 09:56:28 -0600

    Updated browserify to build source bundle and test runner for tests.

--- app/templates/gruntconfig/browserify.js
+++ app/templates/gruntconfig/browserify.js
@@ -2,6 +2,14 @@
 
 var config = require('./config');
 
+// List individual modules here. Each listed module will be aliased in the
+// "bundle", and will be set as an external in the "test".
+var EXPORTS = [
+  // config.src + '/htdocs/js/package/Class.js:package/Class'
+];
+// Subsequent source files can then require "Class" with:
+// var Class = require('package/Class');
+
 var browerify = {
   options: {
     browserifyOptions: {
@@ -12,9 +20,26 @@ var browerify = {
     }
   },
 
+
+  // the bundle used by the index page
   index: {
     src: [config.src + '/htdocs/js/index.js'],
     dest: config.build + '/' + config.src + '/htdocs/js/index.js'
+  },
+
+  // the bundle used by tests
+  bundle: {
+    src: [],
+    dest: config.build + '/' + config.test + '/bundle.js',
+    options: {
+      alias: EXPORTS
+    }
+  },
+
+  // the bundle of test suites
+  test: {
+    src: [config.test + '/test.js'],
+    dest: config.build + '/' + config.test + '/test.js'
   }
 };
 
