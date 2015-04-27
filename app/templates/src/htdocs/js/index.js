822b9b5e2894fbf39c87e81c4d1dc013e2895fce
Eric Martinez <emartinez@usgs.gov>
2015-04-27 13:30:56 -0600

    Created real example module and tests.

--- app/templates/src/htdocs/js/index.js
+++ app/templates/src/htdocs/js/index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/*
-// Potential example...
-var App = require('App');
+var ExampleModule = require('ExampleModule');
 
-App({
-  option1: 'value1',
-  option2: true,
-  option3: 'value3'
-});
-*/
-
-console.log('application running');
+ExampleModule();
